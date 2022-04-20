35e8e6be59c5b432187080a8e1bfd8beb4db085c
pepe lopez <plopez741@gmail.com>
2022-04-20 16:19:41 -0700

    made the review page dynamic

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Login(){
+    return (
+        <>hello world</>
+    )
+}
+
+
+export default Login;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ReviewForm from "./ReviewForm";
 
-function Movie({handleClick}){
+function Movie({handleClick, chosenMovie}){
+
+    console.log(chosenMovie)
+    console.log(chosenMovie.reviews)
+
+
     return(
     <div>
         <button onClick={() => handleClick()} className="backButton">Back</button>
-        <><h1 className="movieReviewsTitle">Movie title</h1></>
-        <div className="movieDescription"><h2  className="movieReviewsTitle">Movie description</h2></div>
-        <div className="reviews" ><h2 className="movieReviewsTitle" >Movie Reviews</h2></div>
+        <><h1 className="movieReviewsTitle">{chosenMovie.name}</h1></>
+        <div className="reviews" ><h2 className="movieReviewsTitle" >{chosenMovie.reviews.map(review => {
+            return (<div key={review.id} >
+            <h3>{review.comment}</h3><h2>{review.rating}</h2>
+            </div>
+            
+        )})}</h2></div>
 
         <><ReviewForm /></>
         

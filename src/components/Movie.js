04a7c115f1d26c0b3d47d0cf483d35d53eda93c7
pepe lopez <plopez741@gmail.com>
2022-04-19 20:08:15 -0700

    added css to movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,10 +4,11 @@ import ReviewForm from "./ReviewForm";
 function Movie({handleClick}){
     return(
     <div>
-        <button onClick={() => handleClick()} >Back</button>
-        <><h1>Movie title</h1></>
-        <div>Movie description </div>
-        <div>Movie Reviews</div>
+        <button onClick={() => handleClick()} className="backButton">Back</button>
+        <><h1 className="movieReviewsTitle">Movie title</h1></>
+        <div className="movieDescription"><h2  className="movieReviewsTitle">Movie description</h2></div>
+        <div className="reviews" ><h2 className="movieReviewsTitle" >Movie Reviews</h2></div>
+
         <><ReviewForm /></>
         
     </div>)

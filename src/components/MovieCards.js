7d03cbac29f07ff7f36504f957a88a9d5ad8bb86
pepe lopez <plopez741@gmail.com>
2022-04-20 18:27:49 -0700

    added images to cards

--- src/components/MovieCards.js
+++ src/components/MovieCards.js
@@ -3,16 +3,16 @@ import will from '../images/will_smith.jpg'
 
 function MovieCards({handleClick, movieInfo}){
 
-    
+    const willl = 'will_smith.jpg'
 
-    
+    console.log(movieInfo)
 
 
     return (<>
         {movieInfo.map(movie => {
             return ( <div key={movie.id} className='card' onClick={() => handleClick(movie.id) }>
             <h3>{movie.name}</h3>
-            <img src={will} alt='img' className='cardImage'/>
+            <img src={require(`../images/${movie.image}`)} alt='img' className='cardImage'/>
             <h5>Average Rating: {movie.avg_rating}/10</h5>
             </div>)
         })}

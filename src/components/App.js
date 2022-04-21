4807c28d8790e6aab28fa73880c903a5b8c95947
pepe lopez <plopez741@gmail.com>
2022-04-21 11:15:51 -0700

    mdae login post

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, {useState} from 'react';
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
 import Home from './Home';
 
 import Login from './Login';
 
 
 function App() {
-    return (<><Router>
-      <Switch>
-      <Route exact path="/">
-        <Login />
+
+  const [hideLogin, setHideLogin] = useState(false)
+  let history = useHistory()
+
+  function handleClick(e){
+    e.preventDefault()
+    
+    setHideLogin(!hideLogin)
+  }
+
+  hideLogin ? history.push('/home') : history.push('/login')
+
+    return (<>
+      
+      <Route exact path="/login">
+        <Login handleClick={setHideLogin} />
       </Route>
       <Route path="/home">
         <Home />
-      </Route></Switch></Router>
+      </Route>
       
       
     </>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
 
-function Login(){
-    return (
-        <>hello world</>
+function Login({handleClick}){
+
+  const [userName, setUserName] = useState('')
+  
+
+  function postUser(){
+
+  
+    fetch('http://localhost:9292/users',{
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({'name': userName})
+    })
+    
+    
+
+    
+
+  }
+
+  
+
+  return (
+      <div>
+        <form onSubmit={e =>{
+          e.preventDefault();
+          postUser()
+          handleClick(true)} } >
+          <label>User Name:
+            <input
+            type="text"
+            placeholder="Insert desired name"
+            onChange={e => setUserName(e.target.value)}
+            />
+          </label>
+          <label>
+            <input type="submit" />
+          </label>
+            </form>
+            <link to="/home" />
+        </div>
     )
 }
 
